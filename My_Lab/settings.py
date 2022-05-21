94090aa989deb9b81bcc10a7a07b66ff31714a04
Afif Imed eddine <73937689+imadlabs@users.noreply.github.com>
2022-05-21 21:47:18 +0100

    make it better V2

--- My_Lab/settings.py
+++ My_Lab/settings.py
@@ -15,7 +15,7 @@ import os
 import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -130,6 +130,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
